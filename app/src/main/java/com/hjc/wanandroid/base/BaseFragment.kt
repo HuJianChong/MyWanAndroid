f5ca33e9228524dbf5821bdad6f1ee46f0f46006
hujianchong <hjc273928@gmail.com>
2022-12-21 16:42:40 +0800

    调整BaseFragment中ViewBinding，防止内存泄漏

--- app/src/main/java/com/hjc/wanandroid/base/BaseFragment.kt
+++ app/src/main/java/com/hjc/wanandroid/base/BaseFragment.kt
@@ -8,14 +8,20 @@ import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
 abstract class BaseFragment<T : ViewBinding> : Fragment() {
-    private lateinit var _binding: T
-    protected val binding get() = _binding
+    private var _binding: T? = null
+    protected val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = getViewBinding(inflater, container)
-        return _binding.root
+        return binding.root
     }
 
     protected abstract fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?): T
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
