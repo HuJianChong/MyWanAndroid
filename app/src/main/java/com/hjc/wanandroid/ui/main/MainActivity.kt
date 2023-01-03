8fccf5dbe327eb6f3a5cf526930eb81bbfdd2058
hujianchong <hjc273928@gmail.com>
2023-01-03 14:37:29 +0800

    首页添加BottomNavigationView

--- app/src/main/java/com/hjc/wanandroid/ui/main/MainActivity.kt
+++ app/src/main/java/com/hjc/wanandroid/ui/main/MainActivity.kt
@@ -1,107 +1,61 @@
 package com.hjc.wanandroid.ui.main
 
 import android.util.Log
-import androidx.core.view.isVisible
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.hjc.wanandroid.R
 import com.hjc.wanandroid.base.BaseActivity
-import com.hjc.wanandroid.base.LoadUiIntent
 import com.hjc.wanandroid.databinding.ActivityMainBinding
-import com.hjc.wanandroid.eventbus.Event
-import com.hjc.wanandroid.eventbus.FlowEventBus
-import com.hjc.wanandroid.ui.adapter.ArticleAdapter
-import com.hjc.wanandroid.ui.adapter.BannerAdapter
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.map
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import com.hjc.wanandroid.ui.main.fragment.BlogFragment
+import com.hjc.wanandroid.ui.main.fragment.MeFragment
+import com.hjc.wanandroid.ui.main.fragment.ProjectFragment
+import com.hjc.wanandroid.ui.main.fragment.SearchFragment
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
-
     companion object {
         private const val TAG = "MainActivity"
     }
 
-    private val mViewModel by viewModel<MainViewModel>()
-    private lateinit var bannerAdapter: BannerAdapter
-    private lateinit var articleAdapter: ArticleAdapter
-
     override fun getViewBinding() = ActivityMainBinding.inflate(layoutInflater)
 
     override fun initViews() {
-        bannerAdapter = BannerAdapter()
-        binding.viewPager.adapter = bannerAdapter
+        binding.navView.setOnItemSelectedListener {
+            Log.d(TAG, "initViews: $it")
+            when (it.itemId) {
+                R.id.navigation_home -> switchFragment(0)
+                R.id.navigation_blog -> switchFragment(1)
+                R.id.navigation_search -> switchFragment(2)
+                R.id.navigation_project_type -> switchFragment(3)
+                R.id.navigation_me -> switchFragment(4)
+            }
+            true
+        }
 
-        articleAdapter = ArticleAdapter()
-        binding.recyclerView.adapter = articleAdapter
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        binding.recyclerView.addItemDecoration(
-            DividerItemDecoration(
-                this, DividerItemDecoration.VERTICAL
-            )
-        )
+        initViewPager()
+    }
+
+    private fun switchFragment(position: Int) = binding.mainViewPager.setCurrentItem(position, true)
+
+    private fun initViewPager() {
+        binding.mainViewPager.isUserInputEnabled = false
+        binding.mainViewPager.offscreenPageLimit = 2
+        binding.mainViewPager.adapter = object : FragmentStateAdapter(this) {
+            override fun getItemCount(): Int = 5
+
+            override fun createFragment(position: Int): Fragment = when (position) {
+                0 -> HomeFragment()
+                1 -> BlogFragment()
+                2 -> SearchFragment()
+                3 -> ProjectFragment()
+                4 -> MeFragment()
+                else -> HomeFragment()
+            }
+
+        }
     }
 
     override fun initEvents() {
-        binding.button.setOnClickListener {
-            mViewModel.sendUiIntent(MainIntent.GetBanner)
-            mViewModel.sendUiIntent(MainIntent.GetDetail(0))
-        }
-
-        lifecycleScope.launchWhenStarted {
-            mViewModel.loadUiIntentFlow.collect { state ->
-                Log.d(TAG, "loadUiStateFlow: $state")
-                when (state) {
-                    is LoadUiIntent.Error -> toast(state.msg)
-                    is LoadUiIntent.ShowMainView -> {
-                        binding.viewPager.isVisible = true
-                        binding.recyclerView.isVisible = true
-                        binding.button.isVisible = false
-                    }
-                    is LoadUiIntent.Loading -> toast("show loading")
-                }
-            }
-        }
-        lifecycleScope.launchWhenStarted {
-            mViewModel.uiStateFlow.map { it.bannerUiState }
-                .collect { bannerUiState ->
-                    Log.d(TAG, "bannerUiState: $bannerUiState")
-                    when (bannerUiState) {
-                        is BannerUiState.INIT -> {}
-                        is BannerUiState.SUCCESS -> {
-                            binding.viewPager.isVisible = true
-                            binding.button.isVisible = false
-                            val imgs = mutableListOf<String>()
-                            for (model in bannerUiState.models) {
-                                imgs.add(model.imagePath)
-                            }
-                            bannerAdapter.setList(imgs)
-                        }
-                    }
-                }
-        }
-        lifecycleScope.launchWhenStarted {
-            mViewModel.uiStateFlow.map { it.detailUiState }
-                .collect { detailUiState ->
-                    Log.d(TAG, "detailUiState: $detailUiState")
-                    when (detailUiState) {
-                        is DetailUiState.INIT -> {}
-                        is DetailUiState.SUCCESS -> {
-                            binding.recyclerView.isVisible = true
-                            val list = detailUiState.articles.datas
-                            articleAdapter.setList(list)
-                        }
-                    }
-
-                }
-        }
-
-        FlowEventBus.observe<Event.ShowInit>(this, Lifecycle.State.STARTED) {
-            it.msg
-            binding.viewPager.isVisible = false
-            binding.recyclerView.isVisible = false
-            binding.button.isVisible = true
-        }
     }
+
+
 }
